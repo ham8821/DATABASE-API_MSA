0a01fd6a43b2780311b3e22ccc95c0d85a65a034
ham8821 <ham38538821@gmail.com>
2019-05-28 12:29:49 +1200

    final submit

---
+++ schoolSIMS/Model/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace schoolSIMS.Model
+{
+    [Table("student")]
+    public partial class Student
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("first_name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Column("last_name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Column("phone_number")]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+        [Column("email")]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [Column("dob", TypeName = "date")]
+        public DateTime? Dob { get; set; }
+        [Column("status")]
+        public byte? Status { get; set; }
+        [Column("date_created", TypeName = "date")]
+        public DateTime? DateCreated { get; set; }
+    }
+}
---
+++ schoolSIMS/Model/schoolsims.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace schoolSIMS.Model
+{
+    public partial class schoolsims : DbContext
+    {
+        public schoolsims()
+        {
+        }
+
+        public schoolsims(DbContextOptions<schoolsims> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:schoolsimsmsa.database.windows.net,1433;Initial Catalog=schoolSIMS;Persist Security Info=False;User ID=msaham8821;Password=msadatabase88!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).IsUnicode(false);
+            });
+        }
+    }
+}
